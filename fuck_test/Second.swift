229086a971232437818d8b6187c53975cd4a230e
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-12 19:54:28 +0300

    second branch - green

---
+++ fuck_test/Second.swift
@@ -0,0 +1,20 @@
+//
+//  Second.swift
+//  fuck_test
+//
+//  Created by Vladimir Syleimanov on 12.01.2021.
+//
+
+import SwiftUI
+
+struct Second: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Second_Previews: PreviewProvider {
+    static var previews: some View {
+        Second()
+    }
+}
---
+++ fuck_test/second2.swift
@@ -0,0 +1,8 @@
+//
+//  second2.swift
+//  fuck_test
+//
+//  Created by Vladimir Syleimanov on 12.01.2021.
+//
+
+import Foundation
